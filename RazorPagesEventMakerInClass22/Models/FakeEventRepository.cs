c9ae188dd3debabb55395bd2f083f43324e07798
Poul Henriksen <pohe@easj.dk>
2022-11-10 11:27:14 +0100

    fake

---
+++ RazorPagesEventMakerInClass22/Models/FakeEventRepository.cs
@@ -0,0 +1,57 @@
+﻿namespace RazorPagesEventMakerInClass22.Models
+{
+    public class FakeEventRepository
+    {
+        private List<Event> events { get; }
+
+        public FakeEventRepository()
+        {
+            events = new List<Event>();
+            events.Add(new Event()
+            {
+                Id = 1,
+                Name = "Roskilde Festival",
+                Description = " A lot of music",
+                City = "Roskilde",
+                DateTime = new DateTime(2020, 6, 9, 10, 0, 0)
+            });
+            events.Add(new Event()
+            {
+                Id = 2,
+                Name = "CPH Marathon",
+                Description = " Many Marathon runners",
+                City = "Copenhagen",
+                DateTime = new DateTime(2020, 3, 6, 9, 30, 0)
+            });
+            events.Add(new Event()
+            {
+                Id = 3,
+                Name = "CPH Distorsion",
+                Description = " A lot of beers",
+                City = "Copenhagen",
+                DateTime = new DateTime(2019, 6, 4, 14, 0, 0)
+            });
+            events.Add(new Event()
+            {
+                Id = 4,
+                Name = "Demo Day",
+                Description = "Project Presentation",
+                City = "Roskilde",
+                DateTime = new DateTime(2020, 6, 9, 9, 0, 0)
+            });
+            events.Add(new Event()
+            {
+                Id = 5,
+                Name = "VM Badminton",
+                Description = "Badminton",
+                City = "Århus",
+                DateTime = new DateTime(2020, 10, 3, 16, 0, 0)
+            });
+        }
+
+        public List<Event> GetAllEvents()
+        {
+            return events.ToList();
+        }
+    }
+}
